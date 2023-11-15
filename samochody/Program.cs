92cb9514d0eba2edbaa67ec410ad7307497e3154
Gruzjan <gracjan122@protonmail.com>
2023-11-15 19:22:13 +0100

    Add project files.

---
+++ samochody/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace samochody
+{
+    public class Samochod
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Rocznik { get; set; }
+
+        public void Informacje()
+        {
+            Console.WriteLine($"Samochód: {Marka} {Model}, Rocznik: {Rocznik}");
+        }
+    }
+}
