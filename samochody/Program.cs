e9bfc153b5850d55b63499f4caa72871c4864ab9
Gruzjan <gracjan122@protonmail.com>
2023-11-16 16:22:37 +0100

    Dodano obsluge trabienia

--- samochody/Program.cs
+++ samochody/Program.cs
@@ -5,10 +5,16 @@
         public string Marka { get; set; }
         public string Model { get; set; }
         public int Rocznik { get; set; }
+        public bool czyPowypadkowy { get; set; }
 
         public void Informacje()
         {
             Console.WriteLine($"Samochód: {Marka} {Model}, Rocznik: {Rocznik}");
+            if (czyPowypadkowy) { Console.WriteLine("Uwaga! Samochoód powypadkowy");  }
+        }
+        public void Zatrab()
+        {
+            Console.WriteLine("Beep! Beep!");
         }
     }
 }
